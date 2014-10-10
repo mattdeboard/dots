f11f17ed2c5e263e384e0f0dea93b590f3f88b42
Matt DeBoard <matt.deboard@gmail.com>
2014-10-10 17:55:19 -0500

    The headers are there!

---
+++ src/cljs/dots/components/board.cljs
@@ -0,0 +1,51 @@
+(ns dots.components.board
+  (:require [dots.dev :refer [is-dev?]]
+            [om.core :as om :include-macros true]
+            [om.dom :as d :include-macros true]))
+
+(defn header-col
+  "Component for individual column headers (i.e. Time and Score)."
+  [props owner]
+  (reify
+    om/IRender
+    (render [_]
+      (let [text (-> props :title clojure.string/capitalize (str " "))
+            elem-id (-> props :title (str "-val"))]
+        (d/div
+         #js {:className "heads"}
+         text
+         (d/span #js {:id elem-id} (:val props)))))))
+
+(defn header
+  "Component for the game board header."
+  [props owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:time 60 :score 0})
+
+    om/IRender
+    (render [_]
+      (let [{:keys [time score]} (om/get-state owner)]
+        (d/div
+         #js {:className "header"}
+         (om/build header-col {:title "time" :val time})
+         (om/build header-col {:title "score" :val score}))))))
+
+(defn board-area [props owner]
+  (reify
+    om/IRender
+    (render [this]
+      (d/div #js {:className "board-area"}
+       (d/div #js {:className "chain-line"}
+        (d/div #js {:className "dot-highlights"}
+         (d/div #js {:className "board"})))))))
+
+(defn game-board [props owner]
+  (reify
+    om/IRender
+    (render [this]
+      (d/div
+       #js {:className "dots-game" :id "main"}
+       (om/build header nil)
+       (om/build board-area nil)))))
--- src/cljs/dots/core.cljs
+++ src/cljs/dots/core.cljs
@@ -1,37 +1,10 @@
 (ns dots.core
-  (:require [dots.dev :refer [is-dev?]]
+  (:require [dots.components.board :refer [game-board]]
+            [dots.dev :refer [is-dev?]]
             [om.core :as om :include-macros true]
             [om.dom :as d :include-macros true]))
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
-
-(defn header [props owner]
-  (reify
-    om/IRender
-    (render [this]
-      (d/div #js {:className "header"}
-             (d/div #js {:className "heads"} "Time "
-                    (d/span #js {:className "time-val"}))
-             (d/div #js {:className "heads"} "Score "
-                    (d/span #js {:className "score-val"}))))))
-
-(defn board-area [props owner]
-  (reify
-    om/IRender
-    (render [this]
-      (d/div #js {:className "board-area"}
-       (d/div #js {:className "chain-line"}
-        (d/div #js {:className "dot-highlights"}
-         (d/div #js {:className "board"})))))))
-
-(defn game-board [props owner]
-  (reify
-    om/IRender
-    (render [this]
-      (d/div
-       #js {:className "dots-game"}
-       (om/build header nil)
-       (om/build board-area nil)))))
+(defonce app-state (atom {}))
 
 (om/root
  (fn [app owner]
@@ -39,4 +12,4 @@
      (render [_]
        (om/build game-board nil))))
  app-state
- {:target (. js/document (getElementById "app"))})
+ {:target (. js/document (getElementById "dots-game-container"))})
