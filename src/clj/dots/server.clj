ce3fcff113898c78e0345f236ee7db1acbba2aef
Matt DeBoard <matt.deboard@gmail.com>
2014-10-13 20:20:31 -0500

    Update server.clj for chestnut to latest.

--- src/clj/dots/server.clj
+++ src/clj/dots/server.clj
@@ -1,13 +1,13 @@
 (ns dots.server
-    (:require [clojure.java.io :as io]
-              [dots.dev :refer [is-dev? inject-devmode-html browser-repl]]
-              [compojure.core :refer [GET defroutes]]
-              [compojure.route :refer [resources]]
-              [compojure.handler :refer [api]]
-              [net.cgrand.enlive-html :refer [deftemplate]]
-              [ring.middleware.reload :as reload]
-              [environ.core :refer [env]]
-              [ring.adapter.jetty :refer [run-jetty]]))
+  (:require [clojure.java.io :as io]
+            [dots.dev :refer [is-dev? inject-devmode-html browser-repl start-figwheel]]
+            [compojure.core :refer [GET defroutes]]
+            [compojure.route :refer [resources]]
+            [compojure.handler :refer [api]]
+            [net.cgrand.enlive-html :refer [deftemplate]]
+            [ring.middleware.reload :as reload]
+            [environ.core :refer [env]]
+            [ring.adapter.jetty :refer [run-jetty]]))
 
 (deftemplate page
   (io/resource "index.html") [] [:body] (if is-dev? inject-devmode-html identity))
@@ -24,8 +24,12 @@
 
 (defn run [& [port]]
   (defonce ^:private server
-    (run-jetty http-handler {:port (Integer. (or port (env :port) 10555))
-                                 :join? false}))
+    (do
+      (if is-dev? (start-figwheel))
+      (let [port (Integer. (or port (env :port) 10555))]
+        (print "Starting web server on port" port ".\n")
+        (run-jetty http-handler {:port port
+                                 :join? false}))))
   server)
 
 (defn -main [& [port]]
